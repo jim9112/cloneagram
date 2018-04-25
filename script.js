400b10860f1ffd08603bca29e8278240bfc39072
jim9112 <jim9112@gmail.com>
2018-04-25 14:52:52 -0400

    Lots of changes

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-window.onscroll = function() {stopScroll()};
 
-
-var header = document.querySelector('.top');
-
-var sticky = header.offsetTop;
-
-function stopScroll() {
-    if (window.pageYOffset >= sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
-  }
+// // script to make the header stay in place
+// window.onscroll = function() {stopScroll()};
+// var header = document.querySelector('.top');
+// var sticky = header.offsetTop;
+// function stopScroll() {
+//     if (window.pageYOffset >= sticky) {
+//       header.classList.add("sticky");
+//     } else {
+//       header.classList.remove("sticky");
+//     }
+//   }
